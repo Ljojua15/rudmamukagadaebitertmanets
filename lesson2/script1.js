30cc0dd7823a2c196e0fe567ccaf8ddea77faa83
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-07-10 20:02:49 +0400

    lesson2

--- lesson2/script1.js
+++ lesson2/script1.js
@@ -1,5 +1,5 @@
 function changeColor(){
-    var hex_number = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
+    var hex_number = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
 
 var hexcode = '';
@@ -9,6 +9,6 @@ for(var i=0; i < 6; i++){
 
     hexcode += hex_number[randomom]
 }
-}
 document.getElementById("randomcolor").innerHTML = hexcode;
 document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
+}
