00084cb4ff264b85e110dceea5a510a93c087441
Ljojua15 <97795530+Ljojua15@users.noreply.github.com>
2023-07-12 12:05:40 +0400

    lesson3

--- lesson2/script1.js
+++ lesson2/script1.js
@@ -2,13 +2,13 @@ function changeColor(){
     var hex_number = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
 
 
-var hexcode = '';
+    var hexcode = '';
 
-for(var i=0; i < 6; i++){
+    for(var i=0; i < 6; i++){
     var randomom = Math.floor(Math.random() * hex_number.length);
 
     hexcode += hex_number[randomom]
-}
-document.getElementById("randomcolor").innerHTML = hexcode;
-document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
+    }
+    document.getElementById("randomcolor").innerHTML = hexcode;
+    document.getElementsByTagName("body")[0].style.background = "#" + hexcode;
 }
---
+++ lesson3/script3.js
@@ -0,0 +1,17 @@
+let contentText
+let authorText
+function getQuote(){
+    fetch("https://api.quotable.io/random")
+        .then(response => response.json())
+        .then(data => {
+    contentText = document.getElementById('text')
+    contentText.innerHTML = data.content
+  
+    authorText = document.getElementById('author')
+    authorText.innerHTML = data.author
+  
+    })
+    .catch(error => {
+        console.log("An error occurred:", error);
+    });
+}
